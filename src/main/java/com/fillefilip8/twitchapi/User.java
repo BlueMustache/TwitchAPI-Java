20ffba481377e7792096857b41a7ed0bc387d0c7
fillefilip8 <fillefilip8@gmail.com>
2015-02-25 14:18:49 +0100

    Fixed some random things

--- src/main/java/com/fillefilip8/twitchapi/User.java
+++ src/main/java/com/fillefilip8/twitchapi/User.java
@@ -10,17 +10,15 @@ import javax.net.ssl.HttpsURLConnection;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
-public class Channel {
-private String channelName;
-private static long followers;
-private static int views;
-	public Channel(String channelName){
-		this.channelName = channelName;
+import com.fillefilip8.twitchapi.Channel;
+public class User {
+private String access_token;
+	public User(String access_token){
+		this.access_token = access_token;
 	}
 	
-	public long getFollowerCount() throws IOException, ParseException{
-		URL url = new URL("https://api.twitch.tv/kraken/channels/" + channelName);
+	public Channel getChannel() throws IOException, ParseException{
+		URL url = new URL("https://api.twitch.tv/kraken?oauth_token=" + access_token);
 		HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
 		// optional default is GET
 		con.setRequestMethod("GET");
@@ -28,24 +26,31 @@ private static int views;
 		//add request header
 		con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36");
 		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'FollowerCount' request for " + channelName + " with URL: " + url);
+		System.out.println("\nSending 'GET' request to URL : " + url);
 		System.out.println("Response Code : " + responseCode);
 
 		BufferedReader in = new BufferedReader(
 		        new InputStreamReader(con.getInputStream()));
+		StringBuffer response = new StringBuffer();
 		JSONParser parser = new JSONParser();
 		JSONObject json = (JSONObject) parser.parse(in);
-		Channel.followers = (long) json.get("followers");
+		
+
+		
 			
 		
 		in.close();
-		
-		return (long) json.get("followers");
-		
-	}
-	public String getChannelName(){
-		return channelName;
-		
-	}
 
+		//print result
+		// get an array from the JSON object
+//		JSONArray streams= (JSONArray) json.get("streams");
+		JSONObject token = (JSONObject) json.get("token");
+
+	    JSONObject channel;
+	    JSONObject name;
+		//JSONArray streamsChannel = (JSONArray) ;
+	   // System.out.println("Username: " + token.get("user_name"));
+		return new Channel(token.get("user_name").toString());
+		
+	}
 }
